8872720eb16d037268acbf35478189931355a97a
soulFood5632 <loganu@shaw.ca>
2023-09-25 10:30:34 -0700

    final update

--- node/move_robot.py
+++ node/move_robot.py
@@ -10,40 +10,84 @@ import numpy as np
 
 
 class CarController:
-    k_p = 10.0
+    k_p = 0.03
     k_d = 0.0
     k_i = 0.0
 
-    speed = 0.5
+    speed = 0.4
+    slow_speed = 0.4
 
+
+    maxError = 400
 
 
     def __init__(self):
         rospy.init_node('controller', anonymous=True)
         self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.bridge = CvBridge()
-        self.subsriber = rospy.Subscriber('rrbot/camera1/raw_image', Image, callback=self.__handleImage)
-        self.loopRate = rospy.Rate(1)
+        self.subsriber = rospy.Subscriber('rrbot/camera1/image_raw', Image, callback=self.__handleImage)
+        self.loopRate = rospy.Rate(10)
         self.last_error = 0
         self.total_error = 0
 
     def listen(self):
+        rospy.loginfo('ready to listen')
         rospy.spin()
         
         
     def __handleImage(self, cameraImage):
-        rospy.loginfo('enetered callback for image processing')
         cvImage = self.bridge.imgmsg_to_cv2(cameraImage)
         error = self.__findError(cvImage)
 
+        rospy.loginfo("firstError = %i", error)
+
+        error = self.__applyClipping(error=error)
+
+        rospy.loginfo("%i", error)
+
+        angularVel = self.__steering_command(error=error)
+
         move = Twist()
-        move.angular.z = self.__steering_command(error=error)
-        move.linear.x = .5
+        move.angular.z = angularVel
+        speed = self.speed
+        if np.abs(angularVel) > 0.8:
+            speed = self.slow_speed
+        move.linear.x = speed
+
+        # rospy.loginfo('%i', angularVel)
 
         self.publisher.publish(move) 
         # didn't think this line was needed based on current assesments of the speed of the control loop. 
 
         # self.loopRate.sleep()
+
+
+
+    def __applyClipping(self, error: int) -> int:
+        # if np.abs(error) > 50:
+        #     rospy.loginfo('exit max turn state')
+        #     self.isMaxLeft = False
+        #     self.isMaxRight = False
+        
+        # if np.abs(error) < 50:
+        #     if abs(self.last_error) > 75:
+        #         if self.last_error < 0:
+        #             rospy.loginfo('max left turn')
+        #             self.isMaxLeft = True
+        #         else:
+        #             rospy.loginfo('max right turn')
+        #             self.isMaxRight = True
+
+
+        
+        # if self.isMaxLeft:
+        #     error = -self.maxError
+        # if self.isMaxRight:
+        #     error = self.maxError
+
+        return error
+
+
 
 
 
@@ -63,7 +107,7 @@ class CarController:
         blurred = cv2.blur(gray, (7, 7), 0)
 
         height, width, _ = cvImg.shape
-        bottom_percentage = 0.2  # Adjust as needed
+        bottom_percentage = 0.32  # Adjust as needed
         crop_height = int(height * bottom_percentage)
 
         cropped_image = blurred[height - crop_height:, :]
@@ -82,11 +126,14 @@ class CarController:
                 largest_centroid_x = int(M["m10"] / M["m00"])
             
             else:
-                largest_centroid_x = 0   
+                largest_centroid_x = 0
         else:
-            largest_centroid_x = 0
-            
-        return largest_centroid_x - width / 2
+            largest_centroid_x = width / 2
+
+        # rospy.loginfo('centroid: %i, width: %i', largest_centroid_x, width / 2)
+
+                    
+        return int(largest_centroid_x - width / 2)
 
 
 
